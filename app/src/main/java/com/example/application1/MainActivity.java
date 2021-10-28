a11f7f683a433b7a552445b4387e272f0e8f3dfe
nkbtemmy <emmanuelnkubito2@gmail.com>
2021-10-28 17:54:03 +0200

    first commit on menyesha project

---
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.application1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivities(new Intent[]{intent});
+                finish();
+            }
+        },5000);
+    }
+}
